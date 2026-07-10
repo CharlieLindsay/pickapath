abb323b3e23f7d2784b0f13e9f975246a707c4e6
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-07-10 21:50:31 +1200

    about to do big structural change with files, added more storyline

---
+++ further_script.js
@@ -0,0 +1,42 @@
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const stageone = document.getElementById("stageone");
+const stagetwo = document.getElementById("stagetwo");
+const stagethree = document.getElementById("stagethree");
+const stagefour = document.getElementById("stagefour");
+const buttons = document.getElementById("buttons");
+let stage = 1;
+stagetwo.style.display = "none";
+stagethree.style.display = "none";
+stagefour.style.display = "none";
+function leftclick() {
+    if (stage == 1) {
+        stage = 2;
+        stageone.style.display = "none";
+        stagetwo.style.display = "block";
+        console.log("Stage 2")
+    }
+    else if (stage == 2) {
+        stagetwo.style.display = "none";
+        stagefour.style.display = "block";
+        buttons.style.display = "none";
+        stage = 1
+    }
+}
+function rightclick() {
+    if (stage == 1) {
+        stage = 3;
+        stageone.style.display = "none";
+        stagethree.style.display = "block";
+        buttons.style.display = "none";
+    }
+    else if (stage == 2) {
+        stage = 3;
+        stagetwo.style.display = "none";
+        stagethree.style.display = "block";
+        buttons.style.display = "none";
+    }
+}
+
+left.addEventListener("click", leftclick);
+right.addEventListener("click", rightclick);
